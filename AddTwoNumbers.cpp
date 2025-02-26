49c22e5a4a231998c404530585baac5ec2e184a3
SpreadSheets600 <Sohammaity006@gmail.com>
2025-02-26 21:07:23 +0530

    Addded AddTwoNumbers.cpp

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a, b;
+    printf("Enter The Value Of A B : ");
+    scanf("%d%d", &a, &b);
+    printf("Sum Of The Given Numbers = %d", a + b);
+    return 0;
+}
