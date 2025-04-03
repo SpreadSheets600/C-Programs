f4efcc23f4510ff3e9fd53419640b66cd6b8264e
SpreadSheets600 <Sohammaity006@gmail.com>
2025-04-03 23:32:25 +0530

    Fix: Improved Matrix Multiplication Logic

--- 05-04-25/MatrixMultiplication.cpp
+++ 05-04-25/MatrixMultiplication.cpp
@@ -1,64 +1,82 @@
 #include <stdio.h>
 int main()
 {
-    int row, column, i, j, k;
+    int row1, col1, row2, col2, i, j, k;
 
-    printf("Enter The Number Of Rows :  ");
-    scanf("%d", &row);
+    printf("Enter The Number Of Rows for First Matrix :  ");
+    scanf("%d", &row1);
 
-    printf("Enter The Number Of Columns :  ");
-    scanf("%d", &column);
+    printf("Enter The Number Of Columns for First Matrix :  ");
+    scanf("%d", &col1);
 
-    int a_1[row][column], a_2[row][column], product[row][column];
+    printf("Enter The Number Of Rows for Second Matrix :  ");
+    scanf("%d", &row2);
 
-    // Matrix 1
-    printf("Enter The Elements Of The First Matrix - \n");
+    printf("Enter The Number Of Columns for Second Matrix :  ");
+    scanf("%d", &col2);
 
-    for (i = 0; i < row; i++)
+    if (row1 != col2)
     {
-        printf("Enter The Row %d Of Matrix : ", i + 1);
-        for (j = 0; j < column; j++)
-            scanf("\n%d", &a_1[i][j]);
+        printf("Matrix Multiplication Is Not Possible\n");
+        return 0;
     }
-
-    printf("The Matrix Is - \n");
-    for (i = 0; i < row; i++)
+    else if (col1 != row2)
     {
-        for (j = 0; j < column; j++)
+        printf("Matrix Multiplication Is Not Possible\n");
+        return 0;
+    }
+    else
+    {
+
+        printf("Matrix Multiplication Is Possible\n");
+
+        int a_1[row1][col1], a_2[row2][col2], product[row1][col2];
+
+        printf("Enter The Elements Of The First Matrix - \n");
+
+        for (i = 0; i < row1; i++)
         {
-            printf(" %d ", a_1[i][j]);
+            printf("Enter The Row %d Of Matrix : ", i + 1);
+            for (j = 0; j < col1; j++)
+                scanf("\n%d", &a_1[i][j]);
         }
-        printf("\n");
-    }
 
-    // Matrix 2
-    printf("Enter The Elements Of The Second Matrix - \n");
-
-    for (i = 0; i < row; i++)
-    {
-        printf("Enter The Row %d Of Matrix : ", i + 1);
-        for (j = 0; j < column; j++)
-            scanf("\n%d", &a_2[i][j]);
-    }
-
-    printf("The Matrix Is - \n");
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < column; j++)
+        printf("The First Matrix Is - \n");
+        for (i = 0; i < row1; i++)
         {
-            printf(" %d ", a_2[i][j]);
-        }
-        printf("\n");
-    }
-
-    // Matrix Multiplication
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < column; j++)
-        {
-            product[i][j] = 0;           // Initialize The Product Matrix To Be 0
-            for (k = 0; k < column; k++) // Iterating The Column
+            for (j = 0; j < col1; j++)
             {
+                printf(" %d ", a_1[i][j]);
+            }
+            printf("\n");
+        }
+
+        // Matrix 2
+        printf("Enter The Elements Of The Second Matrix - \n");
+
+        for (i = 0; i < row2; i++)
+        {
+            printf("Enter The Row %d Of Matrix : ", i + 1);
+            for (j = 0; j < col2; j++)
+                scanf("\n%d", &a_2[i][j]);
+        }
+
+        printf("The Second Matrix Is - \n");
+        for (i = 0; i < row2; i++)
+        {
+            for (j = 0; j < col2; j++)
+            {
+                printf(" %d ", a_2[i][j]);
+            }
+            printf("\n");
+        }
+
+        for (i = 0; i < row1; i++)
+        {
+            for (j = 0; j < col2; j++)
+            {
+                product[i][j] = 0;
+                for (k = 0; k < col1; k++)
                 {
                     product[i][j] += a_1[i][k] * a_2[k][j];
                 }
@@ -66,13 +84,15 @@ int main()
         }
 
         printf("The Product Of The Matrix Is - \n");
-        for (i = 0; i < row; i++)
+        for (i = 0; i < row1; i++)
         {
-            for (j = 0; j < column; j++)
+            for (j = 0; j < col2; j++)
             {
                 printf(" %d ", product[i][j]);
             }
             printf("\n");
         }
+
         return 0;
     }
+}
