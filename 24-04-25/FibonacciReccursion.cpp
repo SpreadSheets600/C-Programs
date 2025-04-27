ce74936a6057b5772d3120aeb8e8f4b403c94184
SpreadSheets600 <sohammaity006@gmail.com>
2025-04-27 09:35:28 +0530

    Added Fibonacci Using Reccursion Program

---
+++ 24-04-25/FibonacciReccursion.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <conio.h>
+
+void fib(int n, int prev1, int prev2)
+{
+    if (n < 3)
+    {
+        return;
+    }
+
+    int curr = prev1 + prev2;
+    printf("%d ", curr);
+
+    return fib(n - 1, prev2, curr);
+}
+
+void printFib(int n)
+{
+
+    if (n < 1)
+    {
+        printf("Invalid Number Of Terms\n");
+    }
+
+    else if (n == 1)
+    {
+        printf("%d ", 0);
+    }
+
+    else if (n == 2)
+    {
+        printf("%d %d", 0, 1);
+    }
+
+    else
+    {
+        printf("%d %d ", 0, 1);
+        fib(n, 0, 1);
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter The Number : ");
+    scanf("%d", &n);
+
+    printFib(n);
+
+    return 0;
+}
