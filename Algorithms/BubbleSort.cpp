cc3b6db399a5cc18048a370951a657d27d519ead
SpeadSheets <sohammaity006@gmail.com>
2025-06-09 12:43:33 +0530

    Feat: Added Bubble Sort

---
+++ Algorithms/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
