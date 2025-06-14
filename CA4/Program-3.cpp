83e11446a186d5b22efdd1154acad5e42b9fbbad
SpreadSheets <sohammaity006@gmail.com>
2025-06-14 14:47:16 +0000

    Feat: CA4 Question 3

---
+++ CA4/Program-3.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int i, len, flag = 0;
+
+    printf("Enter The String : ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] != str[len - i - 1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag)
+        printf("%s Is Not Palindrome", str);
+    else
+        printf("%s Is Palindrome", str);
+
+    return 0;
+}
